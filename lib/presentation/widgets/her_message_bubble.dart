eeee96988b7e3a37f89e753409a709a0c4999f4c
Jose Ramon Bogarin Valenzuela <tec.jrbogarin@gmail.com>
2024-03-06 20:59:18 -0800

    cambios dle dia de ayer

--- lib/presentation/widgets/her_message_bubble.dart
+++ lib/presentation/widgets/her_message_bubble.dart
@@ -21,9 +21,40 @@ class HerMessageBubble extends StatelessWidget {
           ),
         ),
         const SizedBox(
-          height: 20,
+          height: 10,
+        ),
+        const _ImageBubble(),
+        const SizedBox(
+          height: 10,
         )
       ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  const _ImageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+          width: size.width * 0.7,
+          height: size.height * 0.2,
+          fit: BoxFit.cover,
+          "https://yesno.wtf/assets/no/14-cb78bf7104f848794808d61b9cd83eba.gif",
+          loadingBuilder: (context, child, loadingProgress) =>
+              (loadingProgress == null)
+                  ? child
+                  : Container(
+                      width: size.width * 0.7,
+                      height: size.height * 0.2,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 5),
+                      child: const Text("cargando imagen ..."),
+                    )),
     );
   }
 }
